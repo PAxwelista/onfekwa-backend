24fd4df362574a5621d360c23473622e24f3e2e5
Antoine <antoine.devillers01@gmail.com>
2024-10-29 18:22:16 +0100

    Correction schema user + route signin

--- models/users.js
+++ models/users.js
@@ -1,11 +1,29 @@
 const mongoose = require('mongoose'); // Importe mongoose pour gérer les schémas et les modèles dans MongoDB
 
+require('../models/partners')
 // Schéma de l'utilisateur
-const userSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true }, 
-    password: { type: String, required: true } 
+
+
+const bookingsSchema = mongoose.Schema({
+    foreignKey: [{ type: mongoose.Schema.Types.ObjectId, ref: 'partners' }],
+    date: Date,
+    group: String
+   });
+
+const userSchema = mongoose.Schema({
+    firstname: String,
+    lastname: String,
+    email: String, 
+    password:String,
+    phoneNumber: String,
+    favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'partners' }],
+    groups: [String],
+    reservations: bookingsSchema,
+    token: String, 
 });
 
-module.exports = mongoose.model('User', userSchema); 
+User = mongoose.model('users', userSchema); 
+
+module.exports = User;
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
+require('../models/connection');
+const User = require('../models/users');
+const { checkBody } = require('../modules/checkBody');
+
+//token
+const uid2 = require('uid2');
+const token = uid2(32);
+
+//hachage mdp
+const bcrypt = require('bcrypt');
+
+
+router.post('/signup', (req, res) => {
+	if (!checkBody(req.body, ['username', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
+  // Check if the user has not already been registered
+  User.findOne({ username: req.body.username }).then(data => {
+    const hash = bcrypt.hashSync(req.body.password, 10);
+
+    if (data === null) {
+      const newUser = new User({
+        username: req.body.username,
+        password: hash,
+        token,
+      });
+
+      newUser.save().then(() => {
+        res.json({ result: true, token });
+        console.log(token)
+      });
+    } else {
+      // User already exists in database
+      res.json({ result: false, error: 'User already exists' });
+    }
+  });
+});
 
 // Route POST pour la connexion
 router.post('/signin', async (req, res) => {
