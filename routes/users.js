96691b744b27b5feaff09a150d45bb5e18430c56
madotto.Axel@outlook.com <madotto.axel@outlook.com>
2024-11-01 17:42:00 +0100

    nouvelle route pour changer mot de passe

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,28 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const User = require('../models/users');
+const User = require("../models/users");
 
-require('../models/connection');
-const { checkBody } = require('../modules/checkBody');
+require("../models/connection");
+const { checkBody } = require("../modules/checkBody");
 
 //token
-const uid2 = require('uid2');
+const uid2 = require("uid2");
 const token = uid2(32);
 
 //hachage mdp
-const bcrypt = require('bcrypt');
-
+const bcrypt = require("bcrypt");
 
 //#^[a-zA-Z0-9._-]{1,64}@([a-zA-Z0-9-]{2,252}\.[a-zA-Z.]{2,6}){5,255}$# REGEX
 //Route pour l'inscription user par le formulaire
-router.post('/signup', (req, res) => {
-	if (!checkBody(req.body, ['email', 'password', 'firstname', 'lastname'])) {
-    res.json({ result: false, error: 'Champs manquants à remplir' });
-    
+router.post("/signup", (req, res) => {
+  if (!checkBody(req.body, ["email", "password", "firstname", "lastname"])) {
+    res.json({ result: false, error: "Champs manquants à remplir" });
+
     return;
   }
 
   // Check if the user has not already been registered
-  User.findOne({ email: req.body.email }).then(data => {
+  User.findOne({ email: req.body.email }).then((data) => {
     const hash = bcrypt.hashSync(req.body.password, 10);
 
     if (data === null) {
@@ -32,111 +31,145 @@ router.post('/signup', (req, res) => {
         lastname: req.body.lastname,
         email: req.body.email,
         password: hash,
-        groups: ['En couple', 'Avec les collègues', 'Avec les amis', 'En famille'],
+        groups: [
+          "En couple",
+          "Avec les collègues",
+          "Avec les amis",
+          "En famille",
+        ],
         token,
       });
 
       newUser.save().then(() => {
         res.json({ result: true, token });
-        console.log(token)
+        console.log(token);
       });
     } else {
       // User already exists in database
-      res.json({ result: false, error: 'Cet email est déjà utilisé' });
+      res.json({ result: false, error: "Cet email est déjà utilisé" });
     }
   });
 });
 //Route POST pour l'inscription user via google
-router.post('/google', (req, res) => {
+router.post("/google", (req, res) => {
   // Check if the user has not already been registered
-  User.findOne({ email: req.body.username }).then(data => {
+  User.findOne({ email: req.body.username }).then((data) => {
     const hash = bcrypt.hashSync(req.body.password, 10);
 
     if (data === null) {
       const newUser = new User({
         firstname: req.body.fisrtname,
         lastname: req.body.lastname,
-        groups: ['En couple', 'Avec les collègues', 'Avec les amis', 'En famille'],
+        groups: [
+          "En couple",
+          "Avec les collègues",
+          "Avec les amis",
+          "En famille",
+        ],
         token,
       });
 
       newUser.save().then(() => {
         res.json({ result: true, token });
-        console.log(token)
+        console.log(token);
       });
     } else {
       // User already exists in database
-      res.json({ result: false, error: 'Cet email est déjà utilisé' });
+      res.json({ result: false, error: "Cet email est déjà utilisé" });
     }
   });
 });
 
 // Route POST pour la connexion user via l'email
-router.post('/signin', (req, res) => {
-  if (!checkBody(req.body, ['email', 'password'])) {
-    res.json({ result: false, error: 'Champs manquants à remplir' });
+router.post("/signin", (req, res) => {
+  if (!checkBody(req.body, ["email", "password"])) {
+    res.json({ result: false, error: "Champs manquants à remplir" });
     return;
   }
 
-  User.findOne({ email: req.body.email}).then(data => {
+  User.findOne({ email: req.body.email }).then((data) => {
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
-      console.log(data.token)
+      console.log(data.token);
       res.json({ result: true, token: data.token });
     } else {
-      res.json({ result: false, error: 'Aucun utilisateur trouvé' });
+      res.json({ result: false, error: "Aucun utilisateur trouvé" });
     }
   });
 });
 
 // Route pour la connexion avec Google
-router.post('/google', async (req, res) => {
-    const { idToken } = req.body; // Le token reçu du client
+router.post("/google", async (req, res) => {
+  const { idToken } = req.body; // Le token reçu du client
 
-    try {
-        // Vérifier le token Google
-        const ticket = await client.verifyIdToken({
-            idToken,
-            audience: process.env.GOOGLE_CLIENT_ID, // ID client Google
-        });
-        const payload = ticket.getPayload(); // Récupérer les informations de l'utilisateur
+  try {
+    // Vérifier le token Google
+    const ticket = await client.verifyIdToken({
+      idToken,
+      audience: process.env.GOOGLE_CLIENT_ID, // ID client Google
+    });
+    const payload = ticket.getPayload(); // Récupérer les informations de l'utilisateur
 
-        // Vérifier si l'utilisateur existe déjà dans la base de données
-        let user = await findOne({ googleId: payload.sub });
-        if (!user) {
-            // Créer un nouvel utilisateur si non existant
-            user = new user({
-                email: payload.email,
-                googleId: payload.sub,
-                name: payload.name,
-            });
-            await user.save(); // Sauvegarder le nouvel utilisateur
-        }
-
-        // Gérer la session ou générer un token (JWT, par exemple)
-        const token = user.generateAuthToken(); // Méthode pour générer un token
-        res.status(200).json({ message: 'Connexion réussie.', token });
-    } catch (error) {
-        res.status(500).json({ message: 'Erreur du serveur.', error: error.message });
+    // Vérifier si l'utilisateur existe déjà dans la base de données
+    let user = await findOne({ googleId: payload.sub });
+    if (!user) {
+      // Créer un nouvel utilisateur si non existant
+      user = new user({
+        email: payload.email,
+        googleId: payload.sub,
+        name: payload.name,
+      });
+      await user.save(); // Sauvegarder le nouvel utilisateur
     }
+
+    // Gérer la session ou générer un token (JWT, par exemple)
+    const token = user.generateAuthToken(); // Méthode pour générer un token
+    res.status(200).json({ message: "Connexion réussie.", token });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Erreur du serveur.", error: error.message });
+  }
 });
 
 //route qui permet de récupérer les groupes à l'aide du token de l'utilisateur
 router.get("/groups/:token", (req, res) => {
-    const {token} = req.params;
-    User.findOne({token})
-        .then(data=>{
-            if (!data){
-                return res.json({return : false , error : "user not find with this token"})
-            }
-            res.json({return : true , groups : data.groups})
-        })
+  const { token } = req.params;
+  User.findOne({ token }).then((data) => {
+    if (!data) {
+      return res.json({
+        return: false,
+        error: "user not find with this token",
+      });
+    }
+    res.json({ return: true, groups: data.groups });
+  });
+});
 
-})
+router.put("/groups/:token", (req, res) => {
+  const { token } = req.params;
+  const { groups } = req.body;
+  User.updateOne({ token }, { groups }).then(() => res.json({ return: true }));
+});
 
-router.put("/groups/:token" , (req,res)=>{
-    const {token} = req.params;
-    const {groups} = req.body;
-    User.updateOne({token},{groups}).then(()=>res.json({return : true}))
-})
+//route poru cahnger le mot de passe de l'utilisateur
+router.put("/changePassword/:token", (req, res) => {
+  const { token } = req.params;
+  if (!checkBody(req.body, ["newPassword", "oldPassword"])) {
+    res.json({ result: false, error: "Champs manquants à remplir" });
+    return;
+  }
+  
+  User.findOne({ token }).then((data) => {
+    if (data && bcrypt.compareSync(req.body.oldPassword, data.password)) {
+      
+      const hash = bcrypt.hashSync(req.body.newPassword, 10);
+      User.updateOne({ token }, { password: hash }).then((dataUpdate) =>
+        res.json({ result: true})
+      );
+    } else {
+      res.json({ result: false, error: "Ancien mot de passe incorrect" });
+    }
+  });
+});
+
 module.exports = router;
-
